7e1af487552e6fc6b0b36df23285e615f9a9f0e7
unknown <stanjustine@gmail.com>
2023-11-28 11:11:40 +0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div class="default-container">
+    <TheHeader />
+    <slot />
+    <TheFooter />
+  </div>
+</template>
+
+<style>
+
+</style>
